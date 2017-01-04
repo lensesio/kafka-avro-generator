9f5f30fe870cfc118a3d2c729c4fc08baed317af
Christina Daskalaki <chdask@gmail.com>
2017-01-04 17:39:51 +0000

    demo chart

--- avro-kafka-generator/src/main/scala/com/landoop/kafka/Example.scala
+++ avro-kafka-generator/src/main/scala/com/landoop/kafka/Example.scala
@@ -1,5 +1,7 @@
 package com.landoop.kafka
 
+import java.util.{TimerTask, Timer}
+
 import com.github.nscala_time.time.Imports._
 
 import scala.util.{Failure, Random, Success, Try}
@@ -11,21 +13,34 @@ object Example extends App {
 
   val producer = new KProducer[Key, DeviceMeasurements]()
 
-  for (a <- 1 to 1000) {
-    val deviceMeasurement = getMeasurement(-10, 50)
-    val deviceID = getMeasurement(0, 10)
-    val timestamp = DateTime.now().getMillis
+  val t = new Timer()
+  t.schedule(new TimerTask() {
+    @Override
+    def run() {
+      println("posting")
+      produceMessages(Random.nextInt(100))
+    }
+  }, 0, 2000);
 
-    Try(producer.produce("device-measurements-topic", Key(deviceID), DeviceMeasurements(deviceID, deviceMeasurement, "", timestamp))) match {
-      case Success(m) => {
 
-        val metadata = m.get()
-        println("Success writing to Kafka topic:" + metadata.topic(),
-                      metadata.offset(),
-                      metadata.partition(),
-                      new DateTime(metadata.timestamp()))
-       }
-      case Failure(f) => println("Failed writing to Kafka",f)
+
+  def produceMessages(numberOfMessages: Int): Unit = {
+    for (a <- 1 to 1000) {
+      val deviceMeasurement = getMeasurement(-10, 50)
+      val deviceID = getMeasurement(0, 10)
+      val timestamp = DateTime.now().getMillis
+
+      Try(producer.produce("device-measurements-topic", Key(deviceID), DeviceMeasurements(deviceID, deviceMeasurement, "", timestamp))) match {
+        case Success(m) => {
+
+          val metadata = m.get()
+          println("Success writing to Kafka topic:" + metadata.topic(),
+            metadata.offset(),
+            metadata.partition(),
+            new DateTime(metadata.timestamp()))
+        }
+        case Failure(f) => println("Failed writing to Kafka",f)
+      }
     }
   }
 
